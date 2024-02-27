a7e4d328151f067bfbe13b4cf19273268d727201
alexross1787 <alexross1787@gmail.com>
2024-02-26 17:31:04 -0800

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id], id:id })
+    res.render('places/show', { place: places[id], id:id})
 
   }
 })
@@ -57,6 +57,19 @@ router.delete('/:id', (req, res) => {
     places.splice(id, 1)
     
     res.redirect('/places')
+  }
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
   }
 })
 
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,26 +1,32 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('../default.jsx')
 const states = require('D:/UNLV/BE/Rest-rant/models/states.js');
 
-function new_form () {
+function edit_form (data) {
     return (
         <Def>
           <main>
-            <h1>Add a New Place</h1>
-            <form method="POST" action="/places">
+            <h1>Edit Place</h1>
+            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
               <div className="form-group">
                 <label htmlFor="name">Place Name</label>
-                <input className="form-control" id="name" name="name" required />
+                <input 
+                    className="form-control" 
+                    id="name" 
+                    name="name" 
+                    value={data.place.name}
+                    required />
                 </div>
               <div className="form-group">
                 <label htmlFor="pic">Place Picture</label>
                 <input className="form-control" id="pic" name="pic" />
               </div>
-              <div className="form-group">
+              <div className='row'>
+              <div className="form-group col-sm-6">
                 <label htmlFor="city">City</label>
                 <input className="form-control" id="city" name="city" />
               </div>
-              <div className="form-group">
+              <div className="form-group col-sm-6">
                         <label htmlFor="state">State</label>
                         <select className="form-control" id="state" name="state">
                             <option value="">Select State</option>
@@ -29,19 +35,16 @@ function new_form () {
                             ))}
                         </select>
                     </div>
+                    </div>
               <div className="form-group">
                 <label htmlFor="cuisines">Cuisines</label>
                 <input className="form-control" id="cuisines" name="cuisines" required />
               </div>
               <input className="btn btn-primary" type="submit" value="Add Place" />
             </form>
-
-
           </main>
         </Def>
     )
 }
-// module.exports = states;
-module.exports = new_form;
 
-
+module.exports = edit_form
